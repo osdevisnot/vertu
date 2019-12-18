d08c80942d7ad486e8d35537ff24715f58a0d8a3
osdevisnot <osdevisnot@gmail.com>
2019-12-17 22:58:29 -0800

    move to monorepo setup

--- packages/devtools/src/devtools.tsx
+++ packages/devtools/src/devtools.tsx
@@ -1,7 +1,11 @@
-import { store, update } from '../../../src/vertu'
+import { store, update } from 'vertu'
 
-const extension = (window as any).__REDUX_DEVTOOLS_EXTENSION__ || (window as any).top.__REDUX_DEVTOOLS_EXTENSION__
+const extension =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ ||
+  (window as any).top.__REDUX_DEVTOOLS_EXTENSION__
+
 let ignore = false
+
 if (!extension) {
   console.warn('Please install / enable Redux Devtools Extension')
   ;(store as any).devtools = null
@@ -10,7 +14,9 @@ if (!(store as any).devtools) {
   ;(store as any).devtools = extension.connect()
   ;(store as any).devtools.subscribe(message => {
     if (message.type === 'DISPATCH' && message.state) {
-      ignore = message.payload.type === 'JUMP_TO_ACTION' || message.payload.type === 'JUMP_TO_STATE'
+      ignore =
+        message.payload.type === 'JUMP_TO_ACTION' ||
+        message.payload.type === 'JUMP_TO_STATE'
       update(JSON.parse(message.state), message.payload.type)
     }
   })
